7caf7d245028f1a760d304bb67c1913732dfa71b
Avellin003 <uavellinbonaparte@gmail.com>
2024-05-30 08:57:15 +0200

    updates

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,5 +4,11 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator) -> str:
-    """returns the log message obfuscated"""
+    """returns the log message obfuscated with redaction
+    Args:
+        fields: a list of strings representing all fields to obfuscate
+        redaction: a string representing by what the field will be obfuscated
+        message: a string representing the log line
+        separator: a string representing by which character is separating all
+        fields in the log line"""
     return re.sub(separator.join(fields), redaction, message)
