b451313070501c9d074a4c3ff53193ce2054ef0b
Avellin003 <uavellinbonaparte@gmail.com>
2024-06-03 20:04:07 +0200

    new_updates

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Class AUTH
+"""
+from flask import request
+from typing import (
+    List,
+    TypeVar
+)
+
+
+class Auth:
+    """
+    Incapsulates the authentication logic
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Checks if a provided path necessitates authentication.
+        Args:
+            - path (str): The URL path to evaluate.
+            - excluded_paths (List of str): Paths exempted from authentication.
+        Return:
+            - True if the path is not among excluded_paths; otherwise, False."""
+        if path is None:
+            return True
+        elif excluded_paths is None or excluded_paths == []:
+            return True
+        elif path in excluded_paths:
+            return False
+        else:
+            for a in excluded_paths:
+                if a.startswith(path):
+                    return False
+                if path.startswith(i):
+                    return False
+                if a[-1] == "*":
+                    if path.startswith(i[:-1]):
+                        return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Returns the value of the Authorization header in the request
+        """
+        if request is None:
+            return None
+        header = request.headers.get('Authorization')
+        if header is None:
+            return None
+        return header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Returns None: just a placeholder
+        """
+        return None
