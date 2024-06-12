226396ecc6ed3107d7b5b78012f73f915f619636
Avellin003 <uavellinbonaparte@gmail.com>
2024-06-12 14:30:45 +0000

    updates

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 """
 User model for the user authentication service
+
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 # declarative_base is in charge of tables in the database
 """Making an instance of the Base class"""
+
+
 Base = declarative_base()
 
 
