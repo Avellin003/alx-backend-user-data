e515daff1d70b6d90bc7d7a385d970026708ab66
Bonaparte003 <b.uteramaho@alustudent.com>
2024-06-12 16:21:08 +0200

    column table

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+"""
+User model for the user authentication service
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+# declarative_base is in charge of tables in the database
+"""Making an instance of the Base class"""
+Base = declarative_base()
+
+class User(Base):
+    """User class for the user authentication service"""
+    # the __tablename__ attribute defines the name of the table
+    __tablename__ = 'users'
+
+    # the id attribute is a column of the table
+    # it is an integer and can not be null nor repeated (primary_key)
+    id = Column(Integer, primary_key=True)
+    # the email attribute is a column of the table
+    # it is a string, can not be empty
+    email = Column(String, nullable=False)
+    # the hashed_password attribute is a column of the table
+    # it is a string, can not be empty
+    hashed_password = Column(String, nullable=False)
+    # the session_id attribute is a column of the table
+    # it is a string, can be empty
+    session_id = Column(String, nullable=True)
+    # the reset_token attribute is a column of the table
+    # it is a string, can be empty
+    reset_token = Column(String, nullable=True)
