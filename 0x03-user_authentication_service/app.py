06e8b8f0a608637554e097a3c3778844b12d7c7f
Bonaparte003 <b.uteramaho@alustudent.com>
2024-06-12 18:33:28 +0200

    updates

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Flask app for user authentication
+Flask application
 """
 from flask import (
     Flask,
@@ -20,31 +20,31 @@ AUTH = Auth()
 @app.route("/", methods=["GET"], strict_slashes=False)
 def index() -> str:
     """
-    Return a JSON response with a welcome message
-    {"message": "Welcome"}
+    Returns the json response
+    {"message": "Bienvenue"}
     """
-    return jsonify({"message": "Welcome"})
+    return jsonify({"message": "Bienvenue"})
 
 
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def users() -> str:
     """
-    Register new users
+    Registration of new users
     """
     email = request.form.get("email")
     password = request.form.get("password")
     try:
         user = AUTH.register_user(email, password)
     except ValueError:
-        return jsonify({"message": "Email already registered"}), 400
+        return jsonify({"message": "email already registered"}), 400
 
-    return jsonify({"email": f"{email}", "message": "User created"})
+    return jsonify({"email": f"{email}", "message": "user created"})
 
 
 @app.route("/sessions", methods=["POST"], strict_slashes=False)
 def login() -> str:
     """
-    Log in a user with correct credentials and create a new session
+    Log in user credentials provided if correct, and create a new
     """
     email = request.form.get("email")
     password = request.form.get("password")
@@ -53,7 +53,7 @@ def login() -> str:
         abort(401)
 
     session_id = AUTH.create_session(email)
-    resp = jsonify({"email": f"{email}", "message": "Logged in"})
+    resp = jsonify({"email": f"{email}", "message": "logged in"})
     resp.set_cookie("session_id", session_id)
     return resp
 
@@ -61,11 +61,11 @@ def login() -> str:
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout():
     """
-    Log out a user and destroy their session
+    Log out a user based on the ses_id in the received cookies
     """
-    session_id = request.cookies.get("session_id", None)
-    user = AUTH.get_user_from_session_id(session_id)
-    if user is None or session_id is None:
+    ses_id = request.cookies.get("session_id", None)
+    user = AUTH.get_user_from_session_id(ses_id)
+    if user is None or ses_id is None:
         abort(403)
     AUTH.destroy_session(user.id)
     return redirect("/")
@@ -74,10 +74,10 @@ def logout():
 @app.route("/profile", methods=["GET"], strict_slashes=False)
 def profile() -> str:
     """
-    Return a user's email based on their session_id in cookies
+    returns the user's email based ses_id in the received cookies
     """
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
+    ses_id = request.cookies.get("session_id")
+    user = AUTH.get_user_from_session_id(ses_id)
     if user:
         return jsonify({"email": f"{user.email}"}), 200
     abort(403)
@@ -86,7 +86,7 @@ def profile() -> str:
 @app.route("/reset_password", methods=["POST"], strict_slashes=False)
 def get_reset_password_token() -> str:
     """
-    Generate a token to reset a user's password
+    generates a reset token and sends it to the user's email
     """
     email = request.form.get("email")
     try:
@@ -100,7 +100,7 @@ def get_reset_password_token() -> str:
 @app.route("/reset_password", methods=["PUT"], strict_slashes=False)
 def update_password() -> str:
     """
-    Update a user's password
+    performs the password update
     """
     email = request.form.get("email")
     reset_token = request.form.get("reset_token")
